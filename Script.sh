b4c75a3eb760f206265e15d53959c561fbe205bc
kakaa2993@ <kakaa2993@gmail.com>
2022-07-10 19:22:49 -0700

    find the directory of the github repository

--- Script.sh
+++ Script.sh
@@ -3,11 +3,17 @@
 # This script will collect the contributers names and return a list of all the contributers tha contribute to that project in GitHub
 
 path=$(pwd)
-
+#while read line ; do echo ${line##*/} ; done < GitHub_links.txt
 collect_logs(){
-	while read line in GitHub_links.txt
-do 
-	
-
+	while read line
+	do
+		# get the directory of the github repository
+		link="${line##*/}"
+		echo "${link%.*}"
+		#git clone $line
+		#cd "$path/
+	done < GitHub_links.txt
 
 }
+
+collect_logs
