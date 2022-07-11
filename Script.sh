2f38f892b6223fdb2ce721bd7abc8972dd79d222
kakaa2993@ <kakaa2993@gmail.com>
2022-07-10 17:08:04 -0700

    create the main function

--- Script.sh
+++ Script.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+# This script will collect the contributers names and return a list of all the contributers tha contribute to that project in GitHub
+
+path=$(pwd)
+
+collect_logs(){
+	while read line in GitHub_links.txt
+do 
+	
+
+
+}
