2cce63b25619fed5ebbdc1ee4d481cf2e9196f91
kakaa2993@ <kakaa2993@gmail.com>
2022-07-11 18:16:00 -0700

    extract the emails and the names from these logs

--- Script.sh
+++ Script.sh
@@ -8,21 +8,25 @@ touch logs.txt authors.txt
 collect_logs(){
 	while read line
 	do
-		# get the directory of the github repository
-		link="${line##*/}"
-		repo_path="${link%.*}"
+		if [[ -n $line ]]; then
+			# get the directory of the github repository
+			link="${line##*/}"
+			repo_path="${link%.*}"
 
-		#download the repository
-		git clone $line
+			#download the repository
+			git clone $line
 
-		# change the directory to the repository and get the logs of that repository
-		cd "$repo_path"
-		git log >> "${path}/logs.txt"
-		cd "$path"
-		rm -r "$repo_path"
+			# change the directory to the repository and get the logs of that repository
+			cd "${repo_path}"
+			git log >> "${path}/logs.txt"
+			cd "${path}"
+			grep "Author" logs.txt >> authors.txt
 
-		# filter the logs
-		grep "author" logs.txt >> authors.txt
+			echo "${repo_path}"
+			rm -r "${repo_path}"
+
+			# filter the logs
+		fi
 	done < test.txt
 #GitHub_links.txt
 
