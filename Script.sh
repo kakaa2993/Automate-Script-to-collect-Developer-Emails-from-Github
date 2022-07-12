9f1044626680e112fe47f8d8ac36beb7410f309d
kakaa2993@ <kakaa2993@gmail.com>
2022-07-11 17:22:05 -0700

    add comments to better explain the code

--- Script.sh
+++ Script.sh
@@ -4,17 +4,25 @@
 
 path=$(pwd)
 #while read line ; do echo ${line##*/} ; done < GitHub_links.txt
-touch logs.txt
+touch logs.txt authors.txt
 collect_logs(){
 	while read line
 	do
 		# get the directory of the github repository
 		link="${line##*/}"
 		repo_path="${link%.*}"
+
+		#download the repository
 		git clone $line
+
+		# change the directory to the repository and get the logs of that repository
 		cd "$repo_path"
 		git log >> "${path}/logs.txt"
 		cd "$path"
+		rm -r "$repo_path"
+
+		# filter the logs
+		grep "author" logs.txt >> authors.txt
 	done < test.txt
 #GitHub_links.txt
 
